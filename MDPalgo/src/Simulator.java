626c2381223f10ea354e361c5bd5046d245d1b01
iamqingmei <iamqingmei@gmail.com>
2016-09-16 09:01:12 +0800

    merge Simulator Class

--- MDPalgo/src/Simulator.java
+++ MDPalgo/src/Simulator.java
@@ -20,6 +20,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
+import java.util.Scanner;
+
 public class Simulator {
 	// JFrame for the application
 	private static JFrame _appFrame = null;
@@ -329,19 +331,69 @@ public class Simulator {
 		ExplorationAlgo exploration = new ExplorationAlgo(simExMap, simShortestPathMap, bot);
 		exploration.runExploration();
 		runExploration = false;
+		int i = 0;
+		String DescriptorFormat = "11"; //pad the first 2 bits
+		String DescriptorFinal = "";
 		for(int r=0;r<MapConstants.MAP_ROW;r++){//output map descriptor for explored area
 			for(int c=0;c<MapConstants.MAP_COL;c++){
-				if(simExMap.getBlock(r,c).getIsExplored() == true)
-					System.out.println(1);
+				if(simExMap.getBlock(r,c).getIsExplored() == true){					
+					//System.out.println(i++);
+					DescriptorFormat = DescriptorFormat + "1"; //grid explored
+				}
+				else
+					DescriptorFormat = DescriptorFormat + "0"; //grid not explored
+				if(DescriptorFormat.length()==16){
+					int DescriptorFormatBin = Integer.parseInt(DescriptorFormat,2);
+					String DescriptorFormatHex = Integer.toString(DescriptorFormatBin,16);
+					while(DescriptorFormatHex.length()!= DescriptorFormat.length()/4) // pad 0s in front according to string length
+						DescriptorFormatHex = "0"+ DescriptorFormatHex;
+					System.out.println(DescriptorFormatHex);
+					DescriptorFinal += DescriptorFormatHex;
+					DescriptorFormat = "";//resets string after each column
+				}
 			}
 		}
+		DescriptorFormat = DescriptorFormat + "11";//pad the last 2 bits
+		int DescriptorFormatBin = Integer.parseInt(DescriptorFormat,2);
+		String DescriptorFormatHex = Integer.toString(DescriptorFormatBin,16);
+		System.out.println(DescriptorFormatHex);
+		DescriptorFinal += DescriptorFormatHex; //last 4 hexa digits
+		System.out.println(DescriptorFinal);
+		
+		DescriptorFormat = "";
+		String DescriptorFinal2 ="";
 		System.out.println("printing obstacles");
 		for(int r=0;r<MapConstants.MAP_ROW;r++){//output map descriptor for obstacles in map
 			for(int c=0;c<MapConstants.MAP_COL;c++){
-				if(simExMap.getBlock(r,c).getIsObstacle() == true)
-					System.out.println(1);
-			}
+				if(simExMap.getBlock(r,c).getIsExplored() == true){
+					if(simExMap.getBlock(r,c).getIsObstacle() == true)
+						DescriptorFormat += "1";
+					else
+						DescriptorFormat += "0";
+					if(DescriptorFormat.length()==16){
+						System.out.println(DescriptorFormat);
+						DescriptorFormatBin = Integer.parseInt(DescriptorFormat,2);
+						DescriptorFormatHex = Integer.toString(DescriptorFormatBin,16);
+						while(DescriptorFormatHex.length()!= 4)
+							DescriptorFormatHex = "0" + DescriptorFormatHex;//add back the zeros automatically removed by the computer
+						System.out.println(DescriptorFormatHex);
+						DescriptorFinal2 += DescriptorFormatHex;
+						DescriptorFormat = "";//resets string after each column
+					}
+				}
+			}	
 		}
+		System.out.println(DescriptorFormat);
+		while(DescriptorFormat.length()%4!=0) // pad it to make the total digits divisible by 4
+			DescriptorFormat += "1";
+		DescriptorFormatBin = Integer.parseInt(DescriptorFormat,2);
+		DescriptorFormatHex = Integer.toString(DescriptorFormatBin,16);
+		while(DescriptorFormatHex.length()!= DescriptorFormat.length()/4) // pad 0s in front according to string length
+			DescriptorFormatHex = "0"+ DescriptorFormatHex;
+		System.out.println("last df:" + DescriptorFormatHex);
+		DescriptorFinal2 += DescriptorFormatHex; //last 4 hexa digits
+		System.out.println(DescriptorFinal2);
+		
 	}
 
 	private static void timeExploration(){
