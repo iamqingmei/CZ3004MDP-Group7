b0b0ebd82999bb62c047f07faaa0003b451cbb73
iamqingmei <iamqingmei@gmail.com>
2016-09-28 09:54:55 +0800

    compile leaderboard

--- LeaderBoard/src/simulation/Simulator.java
+++ LeaderBoard/src/simulation/Simulator.java
@@ -40,8 +40,6 @@ public class Simulator {
 
 	private static Map simExMap = null;
 	private static Map simShortestPathMap = null;
-	private static Map simTimeExMap = null;
-	private static Map simCoverageExMap = null;
 
 	//for time limited exploration
 	private static int timeLimited=10;
@@ -58,12 +56,6 @@ public class Simulator {
 
 		simExMap = new Map(bot);
 		simExMap.setAllUnexplored();
-
-		simTimeExMap = new Map(bot);
-		simTimeExMap.setAllUnexplored();
-
-		simCoverageExMap = new Map(bot);
-		simCoverageExMap.setAllUnexplored();
 
 		displayEverythings();
 		CommMgr.getCommMgr().setConnection(60000);
@@ -108,8 +100,6 @@ public class Simulator {
 		// Initialize the Map for simulation
 		_mainCards.add(simShortestPathMap, "MAIN");
 		_mainCards.add(simExMap,"EXPLO");
-		_mainCards.add(simTimeExMap,"TIMEEXPLO");
-		_mainCards.add(simCoverageExMap,"COVERAGEEXPLO");
 		
 		CardLayout cl = ((CardLayout) _mainCards.getLayout());
 	    cl.show(_mainCards, "MAIN");
@@ -185,162 +175,8 @@ public class Simulator {
 			}
 		});
 		_mainButtons.add(btn_ShortestPath);
-
-		// Load Map button
-		JButton btn_LoadMap = new JButton("Load Map");
-		btn_LoadMap.setFont(new Font("Arial", Font.BOLD, 13));
-		btn_LoadMap.setFocusPainted(false);
-		btn_LoadMap.addMouseListener(new MouseAdapter() {
-			public void mousePressed(MouseEvent e) {
-				ReadMap(simShortestPathMap);
-			}
-		});
-		_mainButtons.add(btn_LoadMap);
-
-		// Set speed of robot (speed of X steps per second ) button
-		JButton btn_Speed = new JButton("Robot Speed");
-		btn_Speed.setFont(new Font("Arial", Font.BOLD, 13));
-		btn_Speed.setFocusPainted(false);
-		btn_Speed.addMouseListener(new MouseAdapter() {
-			public void mousePressed(MouseEvent e) {
-				JDialog d1=new JDialog(_appFrame,"Change Robot Speed",true);
-				d1.setSize(400,400);
-				d1.setLayout(new FlowLayout());
-				JTextField speedTF = new JTextField(5);
-				JButton speedSaveButton = new JButton("Save");
-				
-				speedSaveButton.addMouseListener(new MouseAdapter() {
-				public void mousePressed(MouseEvent e) {
-						bot.setRobotSpeed(1000/(Integer.parseInt(speedTF.getText())));
-						// System.out.println("botSpeed: " + botSpeed);
-					}
-				});
-		        d1.add(new JLabel("Enter Speed (X steps per second): "));
-		        d1.add(speedTF);
-		        d1.add(speedSaveButton);
-
-		        d1.setVisible(true);
-			}
-		});
-		_mainButtons.add(btn_Speed);
-
-		// for multithreading
-		class timeExploration extends SwingWorker<Integer, String>{
-		    protected Integer doInBackground() throws Exception{
-				bot.setRobotPos(1,1);
-				simTimeExMap.repaint();
-				ExplorationAlgo timeExpo = new ExplorationAlgo(simTimeExMap, simShortestPathMap, bot);
-				timeExpo.runExploration(timeLimited);
-				return 333;
-			}
-		}
-
-		// Time-limited Exploration button
-		JButton btn_TimeExploration = new JButton("Time-limited");
-		btn_TimeExploration.setFont(new Font("Arial", Font.BOLD, 13));
-		btn_TimeExploration.setFocusPainted(false);
-		btn_TimeExploration.addMouseListener(new MouseAdapter() {
-			public void mousePressed(MouseEvent e) {
-				JDialog d2=new JDialog(_appFrame,"Time Limit Exploration",true);
-				d2.setSize(400,400);
-				d2.setLayout(new FlowLayout());
-				JTextField timeTF = new JTextField(5);
-				JButton timeSaveButton = new JButton("Save");
-				
-				timeSaveButton.addMouseListener(new MouseAdapter() {
-				public void mousePressed(MouseEvent e) {
-						timeLimited = (Integer.parseInt(timeTF.getText()));
-						CardLayout cl = ((CardLayout) _mainCards.getLayout());
-					    cl.show(_mainCards, "TIMEEXPLO");
-					    new timeExploration().execute();
-					}
-				});
-
-		        d2.add(new JLabel("Enter time for exploration (in Second): "));
-		        d2.add(timeTF);
-		        d2.add(timeSaveButton);
-
-		        d2.setVisible(true);
-		        
-			}
-		});
-		_mainButtons.add(btn_TimeExploration);
-
-		// for multithreading
-		class coverageExploration extends SwingWorker<Integer, String>{
-		    protected Integer doInBackground() throws Exception{
-				bot.setRobotPos(1,1);
-				simCoverageExMap.repaint();
-
-				ExplorationAlgo coverageExpo = new ExplorationAlgo(simCoverageExMap, simShortestPathMap, bot);
-				coverageExpo.runExploration(coverageLimited);
-				return 444;
-			}
-		}
-		// Coverage-limited Exploration button
-		JButton btn_CoverageExploration = new JButton("Coverage-limited");
-		btn_CoverageExploration.setFont(new Font("Arial", Font.BOLD, 13));
-		btn_CoverageExploration.setFocusPainted(false);
-		btn_CoverageExploration.addMouseListener(new MouseAdapter() {
-			public void mousePressed(MouseEvent e) {
-				JDialog d3=new JDialog(_appFrame,"Coverage Limit Exploration",true);
-				d3.setSize(400,400);
-				d3.setLayout(new FlowLayout());
-				JTextField coverageTF = new JTextField(5);
-				JButton coverageSaveButton = new JButton("Save");
-				
-				coverageSaveButton.addMouseListener(new MouseAdapter() {
-				public void mousePressed(MouseEvent e) {
-						coverageLimited = (Integer.parseInt(coverageTF.getText()));
-						new coverageExploration().execute();
-					}
-				});
-		        d3.add(new JLabel("Enter coverage for exploration ( % of the maze squares): "));
-		        d3.add(coverageTF);
-		        d3.add(coverageSaveButton);
-		        d3.setVisible(true);
-			}
-		});
-		_mainButtons.add(btn_CoverageExploration);
 	}
 
-	private static void ReadMap(Map m){//Map descriptor format
-		System.out.println("starting to read map");
-		try{
-			int decInt, asciiInt;
-			System.out.println("Please enter the name of the MAP you want to use:");
-			Scanner sc = new Scanner(System.in);
-			String file = sc.nextLine();
-			FileReader fr = new FileReader(file + ".txt");
-			// FileReader fr = new FileReader(file + ".txt");
-			BufferedReader br = new BufferedReader(fr);
-				
-					
-				for(int r=MapConstants.MAP_ROW-1;r>=0;r--){
-					for(int c=0;c<MapConstants.MAP_COL;c++){
-						asciiInt = fr.read();
-						if(asciiInt==-1)//check for EOF
-							break;
-						decInt = Character.getNumericValue(asciiInt);
-						// System.out.println(decInt);
-						if(decInt == 1)
-							m.setObstacle(r, c);
-						else if(decInt != 0)//when character in text file is not part of the map eg.spaces etc
-							c--;//empty block has to be assigned 1 or 0 to indicate obstacle
-					}
-				}
-				
-			//}//end of while loop
-		}
-		catch (IOException e) {
-			e.printStackTrace();
-		}
-		 	
-		System.out.println("end of read map method");
-		CardLayout cl = ((CardLayout) _mainCards.getLayout());
-	    cl.show(_mainCards, "MAIN");
-		simShortestPathMap.repaint();
-	}
 
 	private static void mapDescriptor(){
 		String DescriptorFormat = "11"; //pad the first 2 bits
